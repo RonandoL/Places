54d41bbf6e8bae620811fe2a80528f16ff661c07
Ronando Long & Lisa Folliard <student@epicodus.com>
2015-08-11 16:28:21 -0700

    Add app.rb file, index.erb file

--- app.rb
+++ app.rb
@@ -4,5 +4,14 @@ require('./lib/places')     # when copy pasting, change file name
 also_reload('lib/**/*.rb')
 
 get('/') do
+  @print_location = Places.all()
   erb(:index)
+end
+
+get('/results') do
+  locate = params.fetch('our_place')
+  place = Places.new(locate)
+  place.save()
+  @place = place
+  erb(:results)
 end
