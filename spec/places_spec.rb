cd9b9ec413b79c9a197b3b83832a60d1dab53179
Ronando Long & Lisa Folliard <student@epicodus.com>
2015-08-11 14:03:11 -0700

    Add spec and method to test location input

--- spec/places_spec.rb
+++ spec/places_spec.rb
@@ -2,5 +2,11 @@ require('rspec')
 require('places')
 
 describe('Places') do  # create class
+  describe('#location') do
+    it('create a location object') do
+      new_place = Places.new('Paris France')
+      expect(new_place.location()).to(eq('Paris France'))
+    end
+  end
 
 end
